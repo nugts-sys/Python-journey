7551763ba171e0764c29c10683f0dfc97f7971c4
nugts-sys <noethenjonas@gmail.com>
2026-07-20 12:04:38 +0200

    Day 13: finance tracker complete - class, menu, validation, JSON persistence

--- Phase 2/Day 12 Finance Tracker Merge.py
+++ Phase 2/Day 12 Finance Tracker Merge.py
@@ -1,5 +1,7 @@
 #merging the pieces from day 11 to soon have the full project completed
 
+import json
+
 class Expense:
     def __init__ (self, item, amount):
         self.item = item
@@ -8,12 +10,34 @@ class Expense:
     def __str__(self):
         return f"{self.item}: {self.amount}$"
     
-expenses = []
+
+def save_expenses(expenses):
+    expenses_data = []
+    for data in expenses:
+        entry = {"item":data.item,
+                 "amount":data.amount}
+        expenses_data.append(entry)
+    with open("tracker_data.json", "w") as f:
+        json.dump(expenses_data, f)
 
 def add_expense(expenses, item, amount):
         new_expense = Expense(item, amount)
         expenses.append(new_expense)
         return expenses
+
+def load_expenses():
+    loaded_expenses = []
+    with open("tracker_data.json", "r") as f:
+        loaded_expenses = json.load (f)
+    expense_objects = []
+    for raw_data in loaded_expenses:
+        new_expense = Expense(raw_data["item"], raw_data["amount"])
+        expense_objects.append(new_expense)
+    return expense_objects
+
+expenses = load_expenses()
+
+
 
 Interactions = {1:"add expense",
                 2:"exit"}
@@ -25,7 +49,7 @@ print(Interactions)
 def get_valid_amount():
    
     try:
-        amount = int(input("you can add the amount of your choice: "))
+        amount = int(input("you can add the amount of the items cost: "))
     except ValueError:
         amount = -1
 
@@ -53,9 +77,10 @@ while True:
 
     
         elif interaction == 2:
+            save_expenses(expenses)
             break
 
     else:
-        print("error, invalid iption.")
+        print("error, invalid option.")
 
 
