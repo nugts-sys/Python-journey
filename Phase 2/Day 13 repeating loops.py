6cd9a86ecc404a163d52c4d90ff1aba6ed2d74de
nugts-sys <noethenjonas@gmail.com>
2026-07-22 13:22:08 +0200

    Day 13: loop and function repetition - find largest number in list

---
+++ Phase 2/Day 13 repeating loops.py
@@ -0,0 +1,13 @@
+
+
+numbers = [1, 3, 5, 2, 54, 68, 1235, 546, 3 ,6 , 7, 7, 234, 12, 6]
+
+def find_largest(numbers):
+    numbers_max = numbers[0]
+    for number in numbers:
+        if number > numbers_max:
+            numbers_max = number
+    return numbers_max
+
+largest_number = find_largest(numbers)
+print(largest_number)
