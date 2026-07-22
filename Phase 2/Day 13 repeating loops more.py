178f8d0e84681116944b44e444afd33857a85793
nugts-sys <noethenjonas@gmail.com>
2026-07-22 13:36:05 +0200

    Day 13: more loop and function repetition - count even numbers in a list

---
+++ Phase 2/Day 13 repeating loops more.py
@@ -0,0 +1,13 @@
+#another repetition
+
+numbers = [1, 3, 5, 6, 8, 1, 3, 2, 5, 9, 0, 6, 5, 5, 7, 6, 4, 2, 8]
+
+def count_even(numbers):
+    even_numbers = 0
+    for evens in numbers:
+        if evens % 2 == 0:
+            even_numbers += 1
+    return even_numbers
+
+total_even = count_even(numbers)
+print(total_even)
